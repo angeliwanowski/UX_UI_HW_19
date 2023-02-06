370cbe1405f01f7dc97ba36bef5ab263d38a919e
angeliwanowski <angeliwanowski@gmail.com>
2023-02-06 13:27:05 -0500

    Please let images work

---
+++ about.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".desktop").on("click",function(){
+    $(this).addClass('active');
+    $(".stairs").animate({ width: "700px"});
+    });
+    });
+
+
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+$("buhiHouse").click(function(){
+    $("buhiHouse").animate({
+      left: '250px',
+      height: '+=150px',
+      width: '+=150px'
+    });
+  });
