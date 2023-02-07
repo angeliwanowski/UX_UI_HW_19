c41c1c86f604a3c632278ea6ce029921a6c1f947
angeliwanowski <angeliwanowski@gmail.com>
2023-02-06 21:42:26 -0500

    Lets see

--- about.js
+++ about.js
@@ -1,9 +1,23 @@
+// button state change on click
 $(document).ready(function(){
     $(".desktop").on("click",function(){
     $(this).addClass('active');
     $(".stairs").animate({ width: "700px"});
     });
     });
+
+// event listener on clickkkkk
+$(document).ready(function(){
+    $(".tabContent:first").show();
+    $(".tabLinks:first").addClass('active');
+    $(".tabLinks").click(function(event){
+        index = $(this).index();
+        $(".tabLinks").removeClass('active');
+        $(this).addClass('active');
+        $(".tabContent").hide();
+        $(".tabContent").eq(index).show();
+    });
+});
 
 
 
